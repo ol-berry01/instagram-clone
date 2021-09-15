9205fea304d6d2d32da31344b5017ea016c92c50
Oliver Berry <oliver-berry@live.co.uk>
2021-09-15 22:00:10 +0100

    Upload seed file
    Dummy users for dev process

--- src/index.js
+++ src/index.js
@@ -3,3 +3,17 @@ import ReactDOM from "react-dom"
 import App from "./App"
 
 ReactDOM.render(<App />, document.getElementById("root"))
+
+// client-side rendered app: react (cra)
+// database -> Firebase
+// external dependencies -> react loading skeleton
+// styling -> tailwind
+
+// architecture
+// src{
+// -> components,
+// -> constants,
+// -> context,
+// -> helpers,
+// -> lib,
+// -> services}
