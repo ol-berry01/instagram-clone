b56ac48a18bbba3c4b3bf2d492db172317a8eca8
Oliver Berry <oliver-berry@live.co.uk>
2021-09-19 22:15:56 +0100

    Login basic logic
    Update src/pages/Login.js:
    - update React import statement
    - import useHistory
    - import FirebaseContext
    - setup Login logic

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,25 @@
-import React, { useEffect } from "react"
+import React, { useState, useEffect, useContext } from "react"
+import { useHistory } from "react-router-dom"
+
+// Import firebase context
+import FirebaseContext from "../context/firebase"
 
 const Login = () => {
+  const history = useHistory()
+  const { firebase } = useContext(FirebaseContext)
+
+  const [emailAddress, setEmailAddress] = useState("")
+  const [password, setPassword] = useState("")
+
+  const [error, setError] = useState("")
+  const isInvalid = password === "" || emailAddress === ""
+
+  const handleLogin = () => {}
+
+  useEffect(() => {
+    document.title = "Login - My-Instagram"
+  }, [])
+
   return <>I am the login page</>
 }
 
