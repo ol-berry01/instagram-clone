ac915d2b7adc27e30415dd79f3c7c38455e3331e
Oliver Berry <oliver-berry@live.co.uk>
2021-09-19 21:55:43 +0100

    Router & Routes
    Update src/App.js:
    - import lazy from react
    - import Suspense from react
    - import * as ROUTES
    - import Login page
    - update return statement
    
    Create src/constants/Routes.js:
    - declare all route constants
    
    Create src/pages/Login.js:
    - basic Login page with sample return

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import React, { lazy, Suspense } from "react"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+
+import * as ROUTES from "./constants/Routes"
+
+// Pages
+const Login = lazy(() => import("./pages/Login"))
+
 function App() {
-  return <p>Hello</p>
+  return (
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path={ROUTES.LOGIN} component={Login} />
+        </Switch>
+      </Suspense>
+    </Router>
+  )
 }
 
 export default App
---
+++ src/pages/Login.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from "react"
+
+const Login = () => {
+  return <>I am the login page</>
+}
+
+export default Login
